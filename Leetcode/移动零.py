76a09917b007216bb7f3b495aa343777769f15ea
RaysLinn <rayinnz1999@gmail.com>
2021-11-12 17:03:46 +1300

    update moveZeroes

--- Leetcode/移动零.py
+++ Leetcode/移动零.py
@@ -23,14 +23,19 @@ class Solution(object):
             
         # return nums
 
-        index = 0
-        for i in range(len(nums)):
-            if nums[i] != 0:
-                temp = nums[index]
-                nums[index] = nums[i]
-                nums[i] = temp
-                index += 1
-        return nums
+        # index = 0
+        # for i in range(len(nums)):
+        #     if nums[i] != 0:
+        #         temp = nums[index]
+        #         nums[index] = nums[i]
+        #         nums[i] = temp
+        #         index += 1
+        # return nums
+
+        for i in nums:
+            if i == 0:
+                nums.remove(i)
+                nums.append(0)
 
 
 test = Solution()
